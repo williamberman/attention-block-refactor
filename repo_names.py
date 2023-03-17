86df808ebeb6961b0af6fbcfc800aa2e710953c0
William Berman <WLBberman@gmail.com>
2023-03-16 23:19:09 -0700

    fix

--- repo_names.py
+++ repo_names.py
@@ -1,10 +1,34 @@
 from huggingface_hub import HfApi
+import argparse
+import json
 
-api = HfApi()
 
-# pull all diffusers compatible models from hub
-diffusers_models = api.list_models(filter="diffusers")
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-for diffusers_model in diffusers_models:
-    model_id = diffusers_model.modelId
-    print(model_id)
+    parser.add_argument("--hub_uploads_file", required=True, type=str)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    api = HfApi()
+
+    diffusers_models = api.list_models(filter="diffusers")
+
+    model_ids = []
+
+    for diffusers_model in diffusers_models:
+        model_id = diffusers_model.modelId
+
+        model_ids.append(model_id)
+
+    with open(args.hub_uploads_file, "w") as f:
+        json.dump(model_ids, f, indent=4)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
