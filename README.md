9dada9ae312736995fa3bff7bf158927a9b1a78b
Will Berman <WLBberma@gmail.com>
2023-03-17 19:57:19 +0000

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-UNet2DModel: 1036
-VQModel: 12
-AutoencoderKL: 2922
+UNet2DModel: 1039, AttnDownBlock2D: 1033, AttnUpBlock2D: 1033, AttnSkipDownBlock2D: 6, AttnSkipUpBlock2D: 6
+
+VQModel: 13, UNetMidBlock2D: 13, AttnDownEncoderBlock2D: 7, AttnUpDecoderBlock2D: 7
+
+AutoencoderKL: 2927, UNetMidBlock2D: 2927, AttnDownEncoderBlock2D: 1, AttnUpDecoderBlock2D: 1
--- process_outputs.py
+++ process_outputs.py
@@ -21,14 +21,31 @@ def main(args):
     for repo_name, deprecated_classes in outputs.items():
         for deprecated_class in deprecated_classes:
             klass_name = deprecated_class["klass_name"]
+            deprecated_blocks = deprecated_class["deprecated_blocks"]
 
             if klass_name not in processed:
-                processed[klass_name] = 0
-            else:
-                processed[klass_name] += 1
+                processed[klass_name] = {
+                    "count": 0,
+                    "deprecated_blocks": {}
+                }
 
-    for klass_name, count in processed.items():
-        print(f"{klass_name}: {count}")
+            processed[klass_name]["count"] += 1
+
+            for deprecated_block in deprecated_blocks:
+                if deprecated_block not in processed[klass_name]["deprecated_blocks"]:
+                    processed[klass_name]["deprecated_blocks"][deprecated_block] = 0
+
+                processed[klass_name]["deprecated_blocks"][deprecated_block] += 1
+
+    for klass_name, processed_ in processed.items():
+        count = processed_["count"]
+
+        out_str = f"{klass_name}: {count}"
+
+        for deprecated_block, deprecated_block_count in processed_["deprecated_blocks"].items():
+            out_str = f"{out_str}, {deprecated_block}: {deprecated_block_count}"
+
+        print(out_str)
 
 
 if __name__ == "__main__":
